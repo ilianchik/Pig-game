5774fbd381c662f1ae803bdc28d591df24fa31ba
Ilia Kozachyshyn <kozachishin543@gmail.com>
2023-09-29 19:59:35 +0300

    v--3

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 //Statring conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dace functionality
 btnRoll.addEventListener('click', function () {
@@ -30,10 +33,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to next player
     currentScore = 0;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
