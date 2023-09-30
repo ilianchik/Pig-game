bde59dae19cdd903eeacba8c5fe44ae1a3b7931b
Ilia Kozachyshyn <kozachishin543@gmail.com>
2023-09-30 13:35:03 +0300

    final

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -76,4 +76,19 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  playing = true;
+  currentScore = 0;
+  scores = [0, 0];
+  current0El.textContent = currentScore;
+  current1El.textContent = currentScore;
+  score0El.textContent = scores[0];
+  score1El.textContent = scores[1];
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document.querySelector('.player--0').classList.add('player--active');
+  activePlayer = 0;
 });
